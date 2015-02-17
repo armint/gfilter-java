62fe916d202e70e6a2ec90f5747991c00244ae7f
Armin van der Togt <armin@otheruse.nl>
2015-02-17 13:08:02 +0100

    cleanup

--- src/test/java/org/firepick/gfilter/Matrix3x3Test.java
+++ src/test/java/org/firepick/gfilter/Matrix3x3Test.java
@@ -16,21 +16,21 @@ public class Matrix3x3Test {
 	            0, 1, 4,
 	            5, 6, 0);
 		Assert.assertEquals(mat, matCopy);
-		Matrix3x3 matInv = mat.inverse();
-		Assert.assertEquals(new Matrix3x3(-24, 18, 5, 20, -15, -4, -5, 4, 1), matInv);
+		mat.inverse();
+		Assert.assertEquals(new Matrix3x3(-24, 18, 5, 20, -15, -4, -5, 4, 1), mat);
 	}
 
 	@Test
 	public void testInverse() {
-		Matrix3x3 matrix = new Matrix3x3(new double[] { 2, 3, 5, 7, 11, 13, 17, 19, 23 });
-		Matrix3x3 inverse = matrix.inverse();
 		System.out.println("Invert Matrix");
+		Matrix3x3 matrix = new Matrix3x3(new double[] { 2, 3, 5, 7, 11, 13, 17, 19, 23 });
 		System.out.println("Matrix: " + matrix);
-		System.out.println("Inverse: " + inverse);
+		matrix.inverse();
+		System.out.println("Inverse: " + matrix);
 		Matrix3x3 expected = new Matrix3x3(new double[] { -6d / 78, -26d / 78, 16d / 78, -60d / 78, 39d / 78, -9d / 78, 54d / 78, -13d / 78, -1d / 78 });
 		System.out.println("Expected: " + expected);
 		System.out.println("-------------");
-		Assert.assertEquals(expected, inverse);
+		Assert.assertEquals(expected, matrix);
 	}
 	
 
